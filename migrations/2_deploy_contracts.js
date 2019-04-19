5f9163590855e449b9ade66d20410747acadc36f
Joshma91 <josh.ma91@gmail.com>
2019-04-18 22:25:27 -0700

    set up testing dependencies and deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,18 +2,12 @@ const MasterPOC = artifacts.require("MasterPOC");
 const ProofClaim = artifacts.require("ProofClaim");
 const IncomeAssignment = artifacts.require("IncomeAssignment");
 
-module.exports = async (deployer) => {
+module.exports = async deployer => {
   const accounts = await web3.eth.getAccounts();
-  console.log(accounts)
   deployer.deploy(MasterPOC);
-  deployer
+  await deployer
     .deploy(ProofClaim, "PCT", "Proof of Claim Token", accounts[0], 100)
-    .then(pct => {
-      console.log(pct.address);
-      return deployAssignment(deployer, pct.address).then(x => {
-        Promise.resolve();
-      });
-    });
+  await deployer.deploy(IncomeAssignment, ProofClaim.address)
 };
 
 const deployAssignment = (deployer, pctAddress) =>
