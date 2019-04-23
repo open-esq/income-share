0c592d5d885049ec5452ca14835d57bc4f511022
Joshma91 <josh.ma91@gmail.com>
2019-04-22 22:07:43 -0700

    store state of agreement variables

---
+++ client/src/components/AgreementPreview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class AgreementPreview extends React.Component {
+  
+  render () {
+    return (
+      <div>
+        hey there
+      </div>
+    )
+  }
+}
--- client/src/components/AssignmentAgreement.js
+++ client/src/components/AssignmentAgreement.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { APIClient, Openlaw } from "openlaw";
-import { Container, Loader } from "semantic-ui-react";
+import { Container, Loader, Button } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import "openlaw-elements/dist/openlaw-elements.min.css";
 import OpenLawForm from "openlaw-elements";
 import Web3Container from "../utils/Web3Container";
 import IncomeAssignmentContract from "../contracts/IncomeAssignment.json";
+import AgreementPreview from "./AgreementPreview";
 require("dotenv").config();
 
 //create config
@@ -20,21 +22,26 @@ const apiClient = new APIClient(process.env.URL);
 class AssignmentAgreement extends React.Component {
   //initial state of variables for Assignment Template, and web3,etc
   state = {
-    date: "",
-    companyName: "",
-    studentName: "",
-    tokenAddr: "",
-    sellerEthAddr: "",
-    buyerEthAddr: "",
-    price: "",
-    numTransferred: "",
-    sellerAddr: "",
-    buyerAddr: "",
-    buyerEmail: "",
-    sellerEmail: "",
-    web3: null,
-    accounts: null,
-    contract: null,
+
+    // State variables for agreement
+    isaDate: null,
+    companyName: null,
+    studentName: null,
+    tokenAddr: null,
+    numShares: null,
+    effectiveDate: null,
+    numAssignedhares: null,
+    purchasePrice: null,
+    sellerName: null,
+    sellerAddr: null,
+    sellerEthAddr: null,
+    sellerEmail: null,
+    buyerName: null,
+    buyerAddr: null,
+    buyerEthAddr: null,
+    buyerEmail: null,
+  
+    // State variables for OpenLaw
     title: "",
     template: "",
     creatorId: "",
@@ -58,8 +65,6 @@ class AssignmentAgreement extends React.Component {
 
     //pull properties off of JSON and make into variables
     const title = template.title;
-    //set title state
-    this.setState({ title });
 
     //Retreive the OpenLaw Template, including MarkDown
     const content = template.content;
@@ -113,17 +118,66 @@ class AssignmentAgreement extends React.Component {
   };
 
   onChange = (key, value) => {
+    switch(key){
+      case "ISA Date":
+        this.setState({isaDate: value})
+        break;
+      case "Company Name":
+        this.setState({companyName: value})
+        break;
+      case "Student Name":
+        this.setState({studentName: value})
+        break;
+      case "Tokenized Income Share Address":
+        this.setState({tokenAddr: value})
+        break;
+      case "Number of Shares":
+        this.setState({numShares: value})
+        break;
+      case "Effective Date":
+        this.setState({effectiveDate: value})
+        break;
+      case "Number of Assigned Shares":
+        this.setState({numAssignedShares: value})
+        break;
+      case "Purchase Price":
+        this.setState({purchasePrice: value})
+        break;
+      case "Seller Name":
+        this.setState({sellerName: value})
+        break;
+      case "Seller Address":
+        this.setState({sellerAddr: value})
+        break;
+      case "Seller Ethereum Address":
+        this.setState({sellerEthAddr: value})
+        break;
+      case "Seller Signatory Email":
+        this.setState({sellerEmail: value})
+        break;
+        case "Buyer Name":
+        this.setState({buyerName: value})
+        break;
+      case "Buyer Address":
+        this.setState({buyerAddr: value})
+        break;
+      case "Buyer Ethereum Address":
+        this.setState({buyerEthAddr: value})
+        break;
+      case "Buyer Signatory Email":
+        this.setState({buyerEmail: value})
+        break;
+    }
     console.log("KEY:", key, "VALUE:", value);
   };
-  previewTemplate = async event => {};
+  setTemplatePreview = async event => {};
 
   render() {
     const { variables, parameters, executionResult } = this.state;
     if (!executionResult) return <Loader active />;
     return (
       <Container text style={{ marginTop: "7em" }}>
-        <h1>Welcome to React Parcel Micro App!</h1>
-        <p>Hard to get more minimal than this React app.</p>
+        <h1>Openlaw Income Assignment Agreement</h1>
         <OpenLawForm
           apiClient={apiClient}
           executionResult={executionResult}
@@ -132,6 +186,8 @@ class AssignmentAgreement extends React.Component {
           openLaw={Openlaw}
           variables={variables}
         />
+        <Button onClick={this.setTemplatePreview}>Preview</Button>
+        <AgreementPreview />
       </Container>
     );
   }
