bcc89590a728c3af8bf0524f0182241fc8efddcb
Joshma91 <josh.ma91@gmail.com>
2019-04-25 00:18:36 -0700

    add new mock UI elements

--- client/src/components/OwnedTokens.js
+++ client/src/components/OwnedTokens.js
@@ -1,96 +1,73 @@
 import React from "react";
-import {
-  Grid,
-  Table,
-  Segment,
-  Header,
-  Form,
-  Button,
-} from "semantic-ui-react";
+import { Grid, Table, Segment, Header, Form, Button } from "semantic-ui-react";
 
+class OwnedTokens extends React.Component {
+  state={
+    tokenIndex: 1
+  }
 
-const RANKS = [
-  {value: "Private", text : "Private"},
-  {value: "Corporal", text : "Corporal"},
-  {value: "Sergeant", text : "Sergeant"},
-  {value: "Lieutenant", text : "Lieutenant"},
-  {value: "Captain", text : "Captain"},
-  {value: "Major", text : "Major"},
-  {value: "Colonel", text : "Colonel"},
-];
-
-const MECHS = [
-  {value : "WHM-6R", text : "Warhammer WHM-6R"}
-];
-
-const OwnedTokens = () => {
-
-   return (
+  render() {
+    return (
       <div>
-          <Grid>
-              <Grid.Column width={10}>
-                  <Header as="h3">Your Owned Tokens</Header>
-                  <Table celled>
+        <Grid>
+          <Grid.Column width={10}>
+            <Header as="h3">Your Owned Tokens</Header>
+            <Table celled>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell width={5}>
+                    Token Contract Address
+                  </Table.HeaderCell>
+                  <Table.HeaderCell width={3}>
+                    Number of Tokens
+                  </Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>
+                  <a onClick={()=>this.setState({tokenIndex:1})}>0xf290f3d843826d00f8176182fd76550535f6dbb4</a> 
+                    
+                  </Table.Cell>
+                  <Table.Cell>52</Table.Cell>
+                </Table.Row>
 
-                       <Table.Header>
-                          <Table.Row>
-                              <Table.HeaderCell width={5}>
-                                  Token Contract Address
-                              </Table.HeaderCell>
-                              <Table.HeaderCell width={3}>
-                                  Number of Tokens
-                              </Table.HeaderCell>
- 
+                <Table.Row>
+                  <Table.Cell>
+                  <a style={{fontWeight:"bold"}} onClick={()=>this.setState({tokenIndex:2})}>0x04Bb2058E3cfC31721d50DceF96C576C761b38c0</a> 
+                    
+                  </Table.Cell>
+                  <Table.Cell>11</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Header as="h3">Token Details</Header>
+            <Segment>
+              <Form size="large">
+                <span style={{display:"block", paddingBottom:"5px", fontSize:"13px", fontWeight:"bold"}}>0x04Bb2058E3cfC31721d50DceF96C576C761b38c0</span>
+                <Form.Field name="companyName" width={16}>
+                  <label>Company Name</label>
+                  <input disabled value="Lambda Inc." />
+                </Form.Field>
 
-                           </Table.Row>
-                      </Table.Header>
-                      <Table.Body>
-                          <Table.Row>
-                              <Table.Cell>
-                              0xf290f3d843826d00f8176182fd76550535f6dbb4
-                              </Table.Cell>
-                              <Table.Cell>
-                                  52
-                              </Table.Cell>
-                 
-                          </Table.Row>
-                      </Table.Body>
-
-                   </Table>
-              </Grid.Column>
-              <Grid.Column width={6}>
-                  <Header as="h3">Token Details</Header>
-                  <Segment >
-                      <Form size="large">
-                          <Form.Field name="companyName" width={16} >
-                              <label>Company Name</label>
-                              <input
-                                  disabled
-                                  value="Lambda Inc."
-                              />
-                          </Form.Field>
-
-                          <Form.Field name="studentName" width={16} >
-                              <label>Student Name</label>
-                              <input
-                                  disabled
-                                  value="Josh Ma"
-                              />
-                          </Form.Field>
-                          <Form.Field name="ethPaid" width={16} >
-                              <label>ETH Paid to You</label>
-                              <input
-                                  disabled
-                                  value="4.4"
-                              />
-                          </Form.Field>
-                          <Button>Manage Pending Assignment</Button>
-                      </Form>
-                  </Segment>
-              </Grid.Column>
-          </Grid>
+                <Form.Field name="studentName" width={16}>
+                  <label>Student Name</label>
+                  <input disabled value="Josh Ma" />
+                </Form.Field>
+                <Form.Field name="ethPaid" width={16}>
+                  <label>ETH Paid to You</label>
+                  <input disabled value="4.4" />
+                </Form.Field>
+                <Button>Manage Pending Assignment</Button>
+              </Form>
+            </Segment>
+          </Grid.Column>
+        </Grid>
       </div>
-  )
+    );
+  }
 }
 
-export default OwnedTokens
+export default OwnedTokens;
