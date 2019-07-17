66a6b4e76caff0eee418b0261a9aa128a6b1e4fc
joshma91 <josh.ma91@gmail.com>
2019-07-17 12:46:02 -0700

    stashing IPFS working in segments

--- client/src/components/AgreementTemplate.js
+++ client/src/components/AgreementTemplate.js
@@ -5,7 +5,13 @@ import "semantic-ui-css/semantic.min.css";
 import "openlaw-elements/dist/openlaw-elements.min.css";
 import OpenLawForm from "openlaw-elements";
 import AgreementPreview from "./AgreementPreview";
+import IPFS from "ipfs-http-client";
 require("dotenv").config();
+const ipfs = new IPFS({
+  host: "ipfs.infura.io",
+  port: 5001,
+  protocol: "https"
+});
 
 class AgreementTemplate extends React.Component {
   //initial state of variables for Assignment Template, and web3,etc
@@ -27,9 +33,11 @@ class AgreementTemplate extends React.Component {
     // State variables for preview component
     previewHTML: null,
     loading: false,
+    ipfsLoading: false,
     success: false
   };
 
+  fileInputRef = React.createRef();
   componentDidMount = async () => {
     //create config
     const openLawConfig = {
@@ -171,9 +179,32 @@ class AgreementTemplate extends React.Component {
     try {
       //login to api
       this.setState({ loading: true }, async () => {
-        apiClient.login(openLawConfig.userName, openLawConfig.password);
-        console.log("apiClient logged in");
+        
+        const token = await apiClient.login(openLawConfig.userName, openLawConfig.password);
+        const openlaw_jwt = token.headers.openlaw_jwt
+        console.log("OPENLAW_JWT", openlaw_jwt);
 
+
+
+        const res = await fetch("https://pbs.twimg.com/profile_images/1580483969/parisjs_transparent.png")
+        // const res = await fetch(pdfURL, {
+        //   method: 'get',
+        //   headers: new Headers({
+        //     'OPENLAW_JWT': openlaw_jwt
+        //   })
+        // }
+        // );
+        const blob = await res.blob();
+        const reader = new window.FileReader();
+        reader.readAsArrayBuffer(blob);
+        reader.onloadend = async () => {
+          const buffer = await Buffer.from(reader.result);
+          console.log(buffer)
+          const ipfsHash = await ipfs.add(buffer);
+          console.log("IPFS Hash: ", ipfsHash[0].hash);
+        };
+
+        
         //add Open Law params to be uploaded
         const uploadParams = await this.buildOpenLawParamsObj(
           this.state.template,
@@ -197,6 +228,10 @@ class AgreementTemplate extends React.Component {
 
         await apiClient.sendContract([], [], contractId);
 
+        const pdfURL = process.env.URL + "/contract/pdf/" + contractId;
+
+        console.log(pdfURL);
+
         await this.setState({ loading: false, success: true, draftId });
         document.getElementById("success").scrollIntoView({
           behavior: "smooth"
@@ -207,6 +242,26 @@ class AgreementTemplate extends React.Component {
     }
   };
 
+  fileChange = async event => {
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    let reader = new window.FileReader();
+    reader.readAsArrayBuffer(file);
+    reader.onloadend = () => this.convertToBuffer(reader);
+  };
+
+  convertToBuffer = async reader => {
+    this.setState({ ipfsLoading: true }, async () => {
+      //file is converted to a buffer for upload to IPFS
+      const buffer = await Buffer.from(reader.result);
+      //set this buffer -using es6 syntax
+      const ipfsRes = await ipfs.add(buffer);
+      console.log(ipfsRes[0].hash);
+      this.setState({ ipfsLoading: false });
+    });
+  };
+
   render() {
     const {
       apiClient,
@@ -215,7 +270,8 @@ class AgreementTemplate extends React.Component {
       executionResult,
       previewHTML,
       loading,
-      success
+      success,
+      ipfsLoading
     } = this.state;
     if (!executionResult) return <Loader active />;
     return (
@@ -234,6 +290,19 @@ class AgreementTemplate extends React.Component {
           <Button primary loading={loading} onClick={this.onSubmit}>
             Submit
           </Button>
+          <Button
+            content="Upload to IPFS"
+            labelPosition="left"
+            icon="file"
+            loading={ipfsLoading}
+            onClick={() => this.fileInputRef.current.click()}
+          />
+          <input
+            ref={this.fileInputRef}
+            type="file"
+            hidden
+            onChange={this.fileChange}
+          />
         </div>
 
         <Message
